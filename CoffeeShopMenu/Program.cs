1b0037299b6d925a082e30ad2a0903c32883a92c
thinkbook <thinkbook@LAPTOP-I921P866>
2024-03-13 19:10:34 +1300

    Add project files.

---
+++ CoffeeShopMenu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the coffee shop!");
+        Console.WriteLine("Menu: espresso, latte, cappuccino, tea");
+        Console.WriteLine("Please enter your selection: ");
+        string selection = Console.ReadLine().ToLower();
+
+        switch (selection)
+        {
+            case "espresso":
+            case "latte":
+                Console.WriteLine("That will be $3.50, please.");
+                break;
+            case "cappuccino":
+                Console.WriteLine("That will be $4.00, please.");
+                break;
+            case "tea":
+                Console.WriteLine("That will be $2.00, please.");
+                break;
+            default:
+                Console.WriteLine("Invalid selection.");
+                break;
+        }
+    }
+}
